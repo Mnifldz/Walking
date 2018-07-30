0687917d07fe63d3c8bd2f9af6c1e667c54fdd8e
Paul David <paulalexanderdavid@gmail.com>
2018-07-30 16:31:26 -0700

    Editing for HMOG rotation data approximation

--- HMOG_to_Rots.py
+++ HMOG_to_Rots.py
@@ -1,17 +1,14 @@
-# Soft Gyro and Rotations for Aggressive Data
+# Rotations for HMOG Data
 #------------------------------------------------------------------------------
 # Last Updated 7/30/2018
-# Description: This script outputs the gyroscope and rotational data for aggressive
-# walking cycle data.
+# Description: This script outputs rotation matrices from the HMOG data set 
+# taking the gyroscope data as an input.
 
 # Libraries
 #------------------------------------------------------------------------------
-import pandas as pd
 import numpy as np
 import os
 from numpy import linalg as LA
-from isolate_g import get_gravity
-from clean_mag import clean_mag
 
 
 # Small Functions
@@ -34,27 +31,31 @@ Root = "/Users/pauldavid/Documents/My_Files/Applications/Jobs/2018_UNIFYID/Data/
 List = os.listdir(Root)
 L = len(List)
 
-for i in range(13,L):
-    if os.path.isdir(Root + "/" + List[i]):
-        for j in range(24):
-            print("Processing " + List[i] + " session " + str(j+1))
-            name = Root + "/" + List[i] + "/" + List[i] + "_session_" + str(j+1) + "/Gyroscope.csv"
-            
-            Data = np.genfromtxt(name, delimiter = ",")
-            n = Data.shape[0]
-            Rots = [None]*2
-            t_step = [None]*2
-            rotation_data = np.zeros([n, 13])
-            for k in range(n):
-                if k == 0:
-                    Rots[0] = np.identity(3)
-                if k > 0:
-                    t_step = 0.001*(Data[k,0] - Data[k-1,0])
-                    Rots[1] = Rots[0]*so3_exp(t_step*row_to_mat(Data[k, 3:6]))
-                    rotation_data[k,:] = np.append(Data[k,0:3], np.append(Rots[1].reshape([1,9]), Data[k,6])  )
-                    Rots[0] = Rots[1]
-            save_name = Root + "/" + List[i] + "/" + List[i] + "_session_" + str(j+1) + "/Rotations.csv"
-            np.savetxt(save_name, rotation_data, delimiter = ",")
+for i in range(138,L):
+    sub_dir = Root + "/" + List[i]
+    if os.path.isdir(sub_dir):
+        sub_list = os.listdir(sub_dir)
+        for j in range(len(sub_list)):
+            sub_string = sub_list[j].split("_")
+            if "session" in sub_string:
+                print("Processing " + List[i] + " session " + sub_string[2])
+                name = Root + "/" + List[i] + "/" + List[i] + "_session_" + sub_string[2] + "/Gyroscope.csv"
+                
+                Data = np.genfromtxt(name, delimiter = ",")
+                n = Data.shape[0]
+                Rots = [None]*2
+                t_step = [None]*2
+                rotation_data = np.zeros([n, 13])
+                for k in range(n):
+                    if k == 0:
+                        Rots[0] = np.identity(3)
+                    if k > 0:
+                        t_step = 0.001*(Data[k,0] - Data[k-1,0])
+                        Rots[1] = Rots[0]*so3_exp(t_step*row_to_mat(Data[k, 3:6]))
+                        rotation_data[k,:] = np.append(Data[k,0:3], np.append(Rots[1].reshape([1,9]), Data[k,6])  )
+                        Rots[0] = Rots[1]
+                save_name = Root + "/" + List[i] + "/" + List[i] + "_session_" + sub_string[2] + "/Rotations.csv"
+                np.savetxt(save_name, rotation_data, delimiter = ",")
                     
                     
                     
