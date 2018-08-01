d0a7c6e4cf64c281f9641019896bfccfbca714fe
Paul David <paulalexanderdavid@gmail.com>
2018-08-01 15:50:36 -0700

    Soft gyro for HMOG data with statistics and soft gyro toolkit

---
+++ HMOG_SoftGyro.py
@@ -0,0 +1,72 @@
+# Soft Gyroscope Reconstruction for HMOG Data
+#------------------------------------------------------------------------------
+# Last Updated: 8/1/2018
+# Description: We used the accelerometer and magnetometer data from HMOG to 
+# estimate the gyroscope as well as compare it to the actual measured gyroscope 
+# data taken in the HMOG data set.
+
+
+# Libraries
+#------------------------------------------------------------------------------
+import os
+import numpy as np
+import pandas as pd
+import time
+from numpy import genfromtxt
+import soft_gyro_toolkit as sg
+
+# Main Loop for Gyro Reconstruction and Error Analysis
+#------------------------------------------------------------------------------
+Root = "/Users/pauldavid/Documents/My_Files/Applications/Jobs/2018_UNIFYID/Data/HMOG/public_dataset"
+List = os.listdir(Root)
+L = len(List)
+
+master_stats = pd.DataFrame()
+
+for i in range(L):
+    sub_dir = Root + "/" + List[i]
+    if os.path.isdir(sub_dir):
+        sub_list = os.listdir(sub_dir)
+        for j in range(len(sub_list)):
+            sub_string = sub_list[j].split("_")
+            if "session" in sub_string:
+                print("Processing " + List[i] + " session " + sub_string[2])
+                
+                # Import and Pre-Process Data
+                gyro = Root + "/" + List[i] + "/" + List[i] + "_session_" + sub_string[2] + "/Gyroscope.csv"
+                acc  = Root + "/" + List[i] + "/" + List[i] + "_session_" + sub_string[2] + "/Accelerometer.csv"
+                mag  = Root + "/" + List[i] + "/" + List[i] + "_session_" + sub_string[2] + "/Magnetometer.csv"
+                
+                Gyro = genfromtxt(gyro, delimiter = ",")[:,3:6]
+                Acc  = genfromtxt(acc, delimiter = ",")
+                pre_mag  = genfromtxt(mag, delimiter = ",")
+                least = min(Gyro.shape[0], Acc.shape[0], pre_mag.shape[0])
+                
+                Grav = sg.get_gravity(Acc[0:least,3:6])
+                Mag  = sg.clean_mag(pre_mag[0:least,3:6], Grav)
+                Gyro = Gyro[0:least,:]
+                time_stamp = sg.time_diffs(Acc[0:least,0])
+                
+                # Run Soft Gyroscope and Error Analysis
+                start = time.time()
+                soft_Gyro = sg.soft_gyroscope(Grav, Mag)
+                stop = time.time()
+                
+                error = sg.calculate_error(soft_Gyro, Gyro)
+                rmse  = sg.calculate_rmse(soft_Gyro, Gyro)
+                
+                approx_dat = np.concatenate((Acc[0:least,0:3], soft_Gyro, np.reshape(Acc[0:least,6], [least,1])), axis = 1)
+                
+                new_stats_row = pd.DataFrame([[sub_string[0], sub_string[2], str(stop-start)] + [str(error)] + [str(num) for num in rmse]], columns = ["user", "session", "runtime", "error", "rmse_1", "rmse_2", "rmse_3"])
+                
+                master_stats = pd.concat([master_stats, new_stats_row])
+                
+                # Save Soft Gyro Data
+                save_name = Root + "/" + List[i] + "/" + List[i] + "_session_" + sub_string[2] + "/Soft_Gyroscope.csv"
+                np.savetxt(save_name, approx_dat, delimiter = ",")
+                    
+# Save stats
+save_stats = Root + "/Soft_Gyro_Stats.csv"
+master_stats.to_csv(save_stats, sep = ",")
+
+            
---
+++ soft_gyro_toolkit.py
@@ -0,0 +1,186 @@
+# Soft Gyroscope Toolkit
+#------------------------------------------------------------------------------
+# Last Updated 8/1/2018
+# Description: These are all of the tools used for a soft gyroscope from gravity 
+# and magnetometer data.  This includes functions pertaining to gravity extraction
+# from accelerometer data, Lie group computations for gyroscope reconstruction, 
+# and functions associated for error analysis of our resulting approximation.
+
+# Libraries
+#------------------------------------------------------------------------------
+import numpy as np
+from scipy.signal import butter, lfilter, medfilt
+from numpy import linalg as LA
+#import warnings
+
+# Gravity Extraction
+#------------------------------------------------------------------------------
+def butter_lowpass_filter(data, corner=0.02, fs=50, order=3):
+    """
+    Uses signal.butter to perform the filter on data
+    Arguments:
+        <data> (numpy array) --- one dimensional timeseries data
+        which the filter is to be applied to
+        <corner> (float) --- critical (corner) frequency for LPF
+        <fs> (float) --- sampling rate
+        <order> (int) --- represents the order of the filter
+    Returns:
+        <y> (numpy array) --- data which the filter has been applied to
+    """
+
+    # scipy.signal.butter accepts an argument "Wn is normalized from 0 to 1,
+    # where 1 is the Nyquist frequency, pi radians/sample"
+    # the nyquist frequency is fs/2, so we must express Wn as corner / (0.5 * fs)
+
+    Wn = corner / (0.5 * fs)
+    b, a = butter(order, Wn, 'low')
+    
+    return lfilter(b, a, data)
+
+
+def get_gravity(data):
+    '''
+    Takes in the raw accelerometer data as a n x 3 numpy array.
+    Columns x,y,z
+    Implements 2/3 of the technique shown in the following paper:
+    https://hal-upec-upem.archives-ouvertes.fr/hal-00826243/document
+    
+    last 1/3 may be needed
+    
+    https://github.com/KalebKE/AccelerationExplorer/wiki/Low-Pass-Filter-Linear-Acceleration
+    
+    '''
+    data = np.apply_along_axis(butter_lowpass_filter, 0, data)
+    data = np.apply_along_axis(medfilt, 0, data)
+    return data
+
+# Magnetometer Filtering/Cleaning
+#------------------------------------------------------------------------------
+def projection(vec, norm):
+    '''
+    gets the projection of vec onto the plane defined by norm
+    '''
+    norm_norm = norm.dot(norm)
+    proj = vec - ((vec.dot(norm)/norm_norm)*norm)
+    return proj
+    
+
+def clean_mag(mag, g):
+    '''
+    Gets the magnetometer vector that is orthogonal to the gravity vector.
+    Passes it into a median filter.
+    '''
+    #error handling
+    if mag.shape[0] != g.shape[0]:
+        raise TypeError("number of samples of the magnetometer and gravity are not equal")
+        
+    if mag.shape[1] != 3:
+        raise TypeError("magnetometer reading is not in x,y,z form")
+        
+    if g.shape[1] != 3:
+        raise TypeError("gravity vector is not in x,y,z form")
+        
+    for i in range(mag.shape[0]):
+        mag[i,:] = projection(mag[i,:], g[i,:])
+    
+    mag = np.apply_along_axis(medfilt, 0, mag)
+    
+    return mag
+        
+# Lie Group Exponential and Logarithm
+#------------------------------------------------------------------------------
+# SO(3) Log Map
+def SO3_log(Q):
+    """Closed form equation for SO(3) log map."""
+    R = (Q - Q.transpose())/2
+    square = np.matmul(R,R)
+    siz = np.sqrt(-square.trace()/2 )
+    return (np.arcsin(siz)/siz)*R
+
+# Lie algebra exponential map
+def so3_exp(A):
+    """ Lie algebra exponential map for so(3)."""
+    vec = [A[0,1], A[0,2], A[1,2]]
+    theta = LA.norm(vec)
+    return np.identity(3) + (np.sin(theta)/theta)*A + ((1-np.cos(theta))/theta**2)*np.matmul(A,A)
+
+# Row to Mat
+def row_to_mat(vec):
+    """ Takes gyroscope vector and makes it into a Lie algebra element."""
+    return np.array([[0, vec[0], -vec[1]], [-vec[0], 0, vec[2]], [vec[1], -vec[2], 0]])
+
+# Mat to Row
+def mat_to_row(mat):
+    """ Takes Lie algebra-valued matrix and returns corresponding row vector."""
+    return np.array([mat[0,1], -mat[0,2], mat[1,2]])
+
+# Soft Gyroscope Function
+#------------------------------------------------------------------------------
+def time_diffs(times):
+    """ This function takes the absolute time stamps and converts them into 
+        relative time stamps (in seconds)."""
+    L = times.shape[0]
+    time_stamp = []
+    for i in range(L):
+        if i == 0:
+            time_stamp.append(0)
+        else:
+            time_stamp.append( 0.001*(times[i] - times[i-1]) )
+    return time_stamp
+
+def stupid_func(vec):
+    return vec
+            
+
+def soft_gyroscope(Gravity, Magnet):
+    """ Returns a soft gyroscope approximation based on gravity and magnetometer
+        data.  Constructs the matrix and uses a Lie algebra approximation for the 
+        final reconstruction."""
+    L = Gravity.shape[0]
+    C_vec = np.cross(Gravity, Magnet)
+    C_norms = LA.norm(C_vec, axis =1)
+    C_N = np.divide(C_vec, C_norms[:,None])
+    
+    G_norms = LA.norm(Gravity, axis=1)
+    G_N     = np.divide(Gravity, G_norms[:,None])
+    T_G     = np.cross(G_N, C_N)
+    soft_gyro_Lie = np.zeros([L,3])
+    MATS_Lie = [None]*2
+    for i in range(L):
+        if i == 0:    
+            MATS_Lie[0] = np.array([-T_G[i,:], C_N[i,:], G_N[i,:]]).transpose()
+        if i > 0:
+            MATS_Lie[1] = np.array([-T_G[i,:], C_N[i,:], G_N[i,:]]).transpose()
+            Lie_Alg = SO3_log( np.matmul(MATS_Lie[0].transpose(), MATS_Lie[1]) )
+            soft_gyro_Lie[i,:] = mat_to_row(Lie_Alg)
+            MATS_Lie[0] = MATS_Lie[1]  
+    return soft_gyro_Lie
+
+# Error Analysis
+#------------------------------------------------------------------------------
+def calculate_error(a, b):
+    '''
+    a is predicted vectors
+    b are actual vectors
+    '''
+    error_list = []
+    for i in range(a.shape[0]):
+        #Euclidean distance
+        error = LA.norm(a[i, :]-b[i, :])/2
+        error = error/(LA.norm(a[i, :])+1/2)
+        error_list += [error]
+    return sum(error_list)/len(error_list)
+
+def calculate_rmse(a, b):
+    return np.sqrt(np.mean((a-b)**2, axis = 0))
+    
+def calculate_angle_error(a, b):
+    a_n = np.apply_along_axis(lambda row:np.linalg.norm(row,ord=1), 1, a)
+    b_n = np.apply_along_axis(lambda row:np.linalg.norm(row,ord=1), 1, b)
+    a = a/a_n[:,None]
+    b = b/b_n[:,None]
+    return calculate_rmse(a,b)
+
+
+
+
