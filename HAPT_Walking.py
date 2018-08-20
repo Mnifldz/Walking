313b9fd3620c9617df04c8304de18b209dd57247
Paul David <paulalexanderdavid@gmail.com>
2018-08-20 16:41:56 -0700

    Added testing function for classifying data.

--- HAPT_Walking.py
+++ HAPT_Walking.py
@@ -41,7 +41,7 @@ L_test  = gyro_test_x.shape[0]
 
 # Convert/Import Rotations
 #------------------------------------------------------------------------------
-MODE = "Write"  # Modes should either be "Write" or "Import"
+MODE = ""  # Modes should either be "Write" or "Import"
 
 Rots_train_dir = Root_train + "Rotations"
 Rots_test_dir  = Root_test + "Rotations"
@@ -63,8 +63,161 @@ if MODE == "Write":
         Rots_test = wt.gyro_to_rot(gyro_test_x.loc[i,:], gyro_test_y.loc[i,:], gyro_test_z.loc[i,:])
         np.savetxt(Rots_test_dir + "/rots_test_ind=" + str(i) + ".csv", Rots_test, delimiter = ",")
         
+# Train Data
+#------------------------------------------------------------------------------
+def TRAIN(Dir):
+    """Here we train the HAPT data by finding normal distribution corresponding
+       to each data sequence, and then use Fisher information geometry to find
+       the representative normal distribution by averaging each distribution 
+       corresponding to a particular class (i.e. walking, sitting, etc.).  We
+       will subsequently use these meta distributions on the testing data."""
+    
+    rots_dir = Dir + "/Inertial Signals/Rotations"
+    Dir_List = os.listdir(rots_dir)
+    Dir_List = [f for f in Dir_List if os.path.isfile(os.path.join(rots_dir, f))]
+    L = len(Dir_List)
+    
+    Means = np.zeros([L,9])
+    Covs  = np.zeros([L,9])
+    mean_list = []
+    cov_list  = []
+    Labels    = []
+    
+    save_dir = Dir + "/Distributions"
+    if os.path.isdir(save_dir) == 0:
+        os.mkdir(save_dir)
+        
+    
+    # Find Seperate Distributions
+    for i in range(L):
+        print("Finding distribution " + str(i+1) + " of " + str(L))
+        rots = pd.read_csv(rots_dir + "/" + Dir_List[i], delimiter = ",", header = None)
+        
+        new_mean,_,_,_ = wt.SO3_geo_mean(rots, 0.05)
+        new_cov    = wt.SO3_cov(rots, new_mean)
+        Means[i,:] = new_mean.reshape([1,9])
+        Covs[i,:]  = new_cov.reshape([1,9])
+        
+        mean_list += [new_mean]
+        cov_list  += [new_cov]
+        
+        lab_1 = Dir_List[i].find("=")
+        lab_2 = Dir_List[i].find(".")
+        Labels += [ int(Dir_List[i][(lab_1+1):lab_2]) ]
+        
+    # Save individual distributions
+    np.savetxt(save_dir + "/Rots_Individual_Means.csv", Means, delimiter = ",")
+    np.savetxt(save_dir + "/Rots_Individual_Covs.csv", Covs, delimiter = ",")
+    np.savetxt(save_dir + "/Rots_Individual_Labels.csv", Labels, delimiter = ",")
+    
+    
+    # Find Meta-Distributions:
+    train_labels = pd.read_fwf(Dir + "/y_train.txt", header = None)
+    LL = train_labels.shape[0]
+    #LABS = ["WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"]
+    MEAN_GEO = np.zeros([6,9])
+    COV_GEO  = np.zeros([6,9])
+    MEAN_LIN = np.zeros([6,9])
+    COV_LIN  = np.zeros([6,9])
+    for j in range(6):
+        print("Training Class " + str(j+1) + " of 6")
+        sub_inds = [k for k in range(LL) if j == train_labels.loc[k,0]-1]
+        inds     = [int(Labels[k]) for k in sub_inds]
+        
+        mean_class = pd.DataFrame(data = Means[inds,:]).reset_index(drop = True)
+        cov_class  = pd.DataFrame(data = Covs[inds,:]).reset_index(drop = True)
+        
+        print("Finding Geodesic Mean")
+        New_Mean_geo = wt.SO3_geo_mean(mean_class, 0.05)
+        print("Finding Geodesic Covariance")
+        New_Cov_geo = wt.cov_geo_mean(cov_class, 0.05)
+        
+        print("FInding Linear Mean and Covariance")
+        New_Mean_lin = wt.SO3_lin_mean(mean_class)
+        New_Cov_lin  = wt.cov_lin_mean(cov_class)
+        
+        MEAN_GEO[j,:] = New_Mean_geo[0].reshape([1,9])
+        COV_GEO[j,:]  = New_Cov_geo[0].reshape([1,9])
+        MEAN_LIN[j,:] = New_Mean_lin.reshape([1,9])
+        COV_LIN[j,:]  = New_Cov_lin.reshape([1,9])
         
         
+    np.savetxt(save_dir + "/Rots_GeoClass_Means.csv", MEAN_GEO, delimiter = ",")
+    np.savetxt(save_dir + "/Rots_GeoClass_Covs.csv", COV_GEO, delimiter = ",")
+    np.savetxt(save_dir + "/Rots_LinClass_Means.csv", MEAN_LIN, delimiter = ",")
+    np.savetxt(save_dir + "/Rots_LinClass_Covs.csv", COV_LIN, delimiter = ",")
+    return MEAN_GEO, COV_GEO, MEAN_LIN, COV_LIN
+
+
+# Classification
+#------------------------------------------------------------------------------
+def CLASSIFY(MEAN_GEO, COV_GEO, MEAN_LIN, COV_LIN, Dir):
+    """This function classifies the data using the classifier found from the 
+       function 'TRAIN(...)'.  A dataframe with the statistics of the data is 
+       found showing the best linear and geodesic averaging as well as the 
+       actual classification label."""
+       
+    rots_dir = Dir + "/Inertial Signals/Rotations"
+    Dir_List = os.listdir(rots_dir)
+    Dir_List = [f for f in Dir_List if os.path.isfile(os.path.join(rots_dir, f))]
+    L = len(Dir_List)
+    
+    Labels = pd.read_fwf(Dir + "/y_test.txt", header = None)
+    
+    # Reshape All Class Parameters
+    Mean_Lin = [MEAN_LIN[i,:].reshape([3,3]) for i in range(6)]
+    Cov_Lin  = [COV_LIN[i,:].reshape([3,3]) for i in range(6)]
+    Mean_Geo = [MEAN_GEO[i,:].reshape([3,3]) for i in range(6)]
+    Cov_Geo  = [COV_GEO[i,:].reshape([3,3]) for i in range(6)]
+    
+    # Classification Data Frame
+    geo_cols = ["Geo_dist_" + str(i) for i in range(1,7)]
+    lin_cols = ["Lin_dist_" + str(i) for i in range(1,7)]
+    Cols     = geo_cols + lin_cols + ["Lin_Class", "Geo_Class", "Actual_Class", ]
+    CLASSIFIED = pd.DataFrame(columns = Cols)
+    
+    # Classify Testing Data
+    for i in range(L):
+        print("Finding and classifying distribution " + str(i+1) + " of " + str(L))
+        rots = pd.read_csv(rots_dir + "/" + Dir_List[i], delimiter = ",", header = None)
+        
+        
+        
+        new_mean,_,_,_ = wt.SO3_geo_mean(rots,0.05)
+        new_cov        = wt.SO3_cov(rots, new_mean)
+        new_row        = []
+        
+        for s in range(6):
+            new_row += [np.sqrt(wt.SO3_dist(new_mean,Mean_Geo[s])**2 + wt.SPD_dist(new_cov, Cov_Geo[s])**2 ) ]
+        for s in range(6):
+            new_row += [ np.sqrt(wt.SO3_dist(new_mean, Mean_Lin[s])**2 + wt.SPD_dist(new_cov, Cov_Lin[s])**2 ) ]
+            
+        new_row += [new_row[:6].index(min(new_row[:6]))]
+        new_row += [new_row[6:].index(min(new_row[6:]))]
+        new_row += [Labels.loc[i,0]]
+        
+        new_row = pd.DataFrame(data = [new_row], columns = Cols)
+        CLASSIFIED = pd.concat([CLASSIFIED, new_row], ignore_index = True )
+        
+    CLASSIFIED.to_csv(Dir + "/Classified_Data.csv", index = False)
+    return CLASSIFIED
+        
+MEAN_GEO, COV_GEO, MEAN_LIN, COV_LIN = TRAIN("/Volumes/Seagate Backup Plus Drive/UnifyID/HAPT/UCI HAR Dataset/train")    
+STATS                                = CLASSIFY(MEAN_GEO, COV_GEO, MEAN_LIN, COV_LIN, "/Volumes/Seagate Backup Plus Drive/UnifyID/HAPT/UCI HAR Dataset/test")           
+        
+        
+        
+        
+
+    
+    
+
+
+        
+                
+        
+                
+            
         
         
         
--- walking_toolkit.py
+++ walking_toolkit.py
@@ -8,7 +8,9 @@
 # Libraries
 #------------------------------------------------------------------------------
 import numpy as np
+import numpy.linalg as npl
 import scipy.linalg as LA
+import time
 
 
 
@@ -56,8 +58,105 @@ def gyro_to_rot(x,y,z):
     
     return Rots
 
-def stupid_func(vec):
-    return np.zeros([128,9])
+# SO(3) Distance Function
+def SO3_dist(A,B):
+    """ Returns the Lie group distance of two rotation matrices."""
+    return LA.norm( SO3_log( np.matmul(A.transpose(), B))  )
+
+# SPD(n) Distance Function
+def SPD_dist(A,B):
+    """ Returns the affine-invariant distance of two SPD matrices (i.e. covariances)."""
+    pre_dist = npl.solve( LA.sqrtm(A), B)
+    return LA.norm( LA.logm( npl.solve( LA.sqrtm(A), pre_dist.transpose()   )  )  )
+
+# Averaging/Functions for Minimizing Mean-Squared Distance
+#------------------------------------------------------------------------------
+# SO(3) mean via mean-squared distance
+def SO3_geo_mean(Rotations,thresh):
+    """ This function will find the barycenter of N rotations via a gradient 
+        descent method and Lie group-valued computations.  The input 'Rotations'
+        is expected to be an Nx9 data frame where each row is a flattened (i.e.
+        vectorized) rotation matrix."""
+    L = Rotations.shape[0]
+    Rots = [Rotations.loc[i,:].values.reshape([3,3]) for i in range(L)]
+    
+    # Gradient Descent Algorithm
+    current = SO3_lin_mean(Rotations) # Initial guess
+    tan     = np.array([[0,1,0], [-1,0,0], [0,0,0]]) # Initial Guess for tangent vector
+    siz     = 0.01 # step size of the algorithm 
+    iters   = 0
+    
+    start = time.time()
+    while LA.norm(np.matmul(current.transpose(), tan)) > thresh:
+        if time.time() - start >= 60:
+            break
+        current = np.matmul(current, so3_exp(-siz* tan))
+        Log_Mats = [SO3_log(np.matmul(x.transpose(), current )) for x in Rots]
+        Log_Mats = [A for A in Log_Mats if ~np.isnan(A).any() and ~np.isinf(A).any()]
+        tan =  sum(Log_Mats)
+        iters += 1       
+     
+    return current,LA.norm(np.matmul(current.transpose(), tan)), iters+1, time.time() - start
+
+# SO(3) mean via Log-Euclidean averaging
+def SO3_lin_mean(Rotations):
+    """Finds the average of rotations matrices via the Log-Euclidean framework."""
+    L    = Rotations.shape[0]
+    Rots = [Rotations.loc[i,:].values.reshape([3,3]) for i in range(L)]
+    Logs = [SO3_log(A) for A in Rots]
+    Logs = [A for A in Logs if ~np.isinf(A).any()]
+    Logs = [A for A in Logs if ~np.isnan(A).any()]
+    L    = len(Logs)
+    return so3_exp((1/L)*sum(Logs))
+
+# Find Lie Group Covariance from Gaussian model
+def SO3_cov(Rotations,mean):
+    """ This function finds the Lie group covariance of the rotations.  This
+        requires the input of 'mean' as found in the function 'find_mean' as 
+        given above."""
+        
+    L = Rotations.shape[0]
+    Rots = [Rotations.loc[i,:].values.reshape([3,3]) for i in range(L)]
+    
+    Vecs = [mat_to_row(SO3_log( np.matmul(mean.transpose(), x ))) for x  in Rots]
+    Mats = [np.outer(Vecs[i], Vecs[i]) for i in range(L)]
+    return (1/L)*sum(Mats)
+
+# Find the Mean Covariance
+def cov_geo_mean(cov_seq, thresh):
+    """ This function finds the SPD-valued mean of the covariances, interpreting
+        the mean of these matrice as the minimizer of the mean-squared distance
+        in the manifold SPD(n) with the affine-invariant metric (coinciding
+        with the Fisher information metric)."""
+    
+    # Gradient Descent Algorithm
+    L       = cov_seq.shape[0]
+    Covs    = [cov_seq.loc[i,:].values.reshape([3,3]) for i in range(L)]
+    current = cov_lin_mean(cov_seq)
+    tan     = np.array([[0,1,0], [1,0,0], [0,0,0]])
+    siz     = 0.01
+    iters   = 0
+    
+    start = time.time()
+    while LA.norm(tan) > thresh:
+        if time.time() - start > 15:
+            break
+        pre_tan = npl.solve(LA.sqrtm(current), tan)
+        tan     = npl.solve(LA.sqrtm(current), pre_tan.transpose())
+        current = np.matmul( LA.sqrtm(current), np.matmul( LA.expm(-siz*tan), LA.sqrtm(current)  ))
+        Log_Mats = [LA.logm( npl.solve(C, current )) for C in Covs]
+        tan      = np.matmul(current, sum(Log_Mats))
+        iters += 1
+    
+    return current, LA.norm(tan), iters+1, time.time() - start
+        
+# Find linear average of covariance matrices
+def cov_lin_mean(cov_seq):
+    """This function finds the linear mean of covariance matrices."""
+    L = cov_seq.shape[0]
+    Covs = [cov_seq.loc[i,:].values.reshape([3,3]) for i in range(L)]
+    return(1/L)*sum(Covs)
+    
 
 
 
